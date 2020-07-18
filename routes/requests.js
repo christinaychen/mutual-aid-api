7c3ce6666482f2e9bc833fca9bf8f23e827ee556
christinachen4 <cchen4@atlassian.com>
2020-07-18 16:39:18 -0400

    added GET endpoints for housing and material

--- routes/requests.js
+++ routes/requests.js
@@ -24,13 +24,76 @@ router.get('/requests/financial', async function(err, res){
         var jsonData = {}
         jsonData.date = data[i][0]
         jsonData.name = data[i][1]
-        jsonData.phone = data[i][2]
-        jsonData.email = data[i][3]
-        jsonData.amount = data[i][4]
-        jsonData.fundTo = data[i][5]
-        jsonData.payMethod = data[i][6]
-        jsonData.extraInfo = data[i][7]
-        jsonData.pronouns = data[i][8]
+        jsonData.pronouns = data[i][2]
+        jsonData.phone = data[i][3]
+        jsonData.email = data[i][4]
+        jsonData.amount = parseInt(data[i][5])
+        jsonData.fundTo = data[i][6]
+        jsonData.payMethod = data[i][7]
+        jsonData.extraInfo = data[i][8]
+
+        ret.requests.push(jsonData)
+    }
+    return res.json(ret)
+})
+
+
+router.get('/requests/material', async function(err, res){
+    console.log(require('dotenv').config())
+    res.status(200)
+    res.setHeader('Content-Type', 'application/json');
+    const spreadsheet = await getSpreadSheetValues({
+        spreadsheetId: "1ytR65k2UFqVecAuTz2o5hINGvONkmEAh2EYEOO9z4zM",
+        auth: process.env.API_KEY,
+        range: "Material"
+    }).catch(err)
+    data = spreadsheet.data.values
+    ret = {
+        requests: []
+    }
+    for (var i =1; i<data.length; i++) {
+        var jsonData = {}
+        jsonData.date = data[i][0]
+        jsonData.name = data[i][1]
+        jsonData.email = data[i][2]
+        jsonData.pronouns = data[i][3]
+        jsonData.affiliation = data[i][4]
+        jsonData.materialRequested = data[i][5]
+        jsonData.description = data[i][6]
+        jsonData.requestOption = data[i][7]
+        jsonData.phone = data[i][8]
+
+        ret.requests.push(jsonData)
+    }
+    return res.json(ret)
+})
+
+router.get('/requests/housing', async function(err, res){
+    console.log(require('dotenv').config())
+    res.status(200)
+    res.setHeader('Content-Type', 'application/json');
+    const spreadsheet = await getSpreadSheetValues({
+        spreadsheetId: "1ytR65k2UFqVecAuTz2o5hINGvONkmEAh2EYEOO9z4zM",
+        auth: process.env.API_KEY,
+        range: "Housing"
+    }).catch(err)
+    data = spreadsheet.data.values
+    ret = {
+        requests: []
+    }
+    for (var i =1; i<data.length; i++) {
+        var jsonData = {}
+        jsonData.date = data[i][0]
+        jsonData.name = data[i][1]
+        jsonData.housingType = data[i][2]
+        jsonData.genderIdentity = data[i][3]
+        jsonData.race = data[i][4]
+        jsonData.accessibilityNeeds = data[i][5]
+        jsonData.allergies = data[i][6]
+        jsonData.immunocompromisedNeeds = data[i][7]
+        jsonData.coronavirusExposure = data[i][8]
+        jsonData.housingStartDate = data[i][9]
+        jsonData.housingEndDate = data[i][10]
 
         ret.requests.push(jsonData)
     }
@@ -38,3 +101,4 @@ router.get('/requests/financial', async function(err, res){
 })
 
 module.exports = router
+
